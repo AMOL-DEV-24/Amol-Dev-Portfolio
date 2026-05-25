d032a5b22fe278d7ad3579049ebbf5d44f4494b9
Amol Pawar <amolpawar.dev@gmail.com>
2026-05-25 16:08:39 +0530

    Seperated the Project data

---
+++ src/data/projects.ts
@@ -0,0 +1,94 @@
+const projectsData = [
+  {
+    image: "/assets/images/projects/Gym App.png",
+    title: "GYM APP",
+    description:
+      "This is Gym App developed using ReactJs for practice purpose to understand how React works.",
+    liveDemo: "https://gym-app-three-rho.vercel.app/",
+    code: "https://github.com/2404AMOL/GYM-APP",
+  },
+
+  {
+    image: "/assets/images/projects/Bike App.png",
+    title: "BIKE APP",
+    description:
+      "This is Bike WebApp which provides booking your favourite bike and viewing latest bikes. It also provides renting bikes at affordable prices.",
+    liveDemo: "https://bike24.netlify.app/",
+    code: "https://github.com/24Amol/BIKE-WEBAPP24",
+  },
+
+  {
+    image: "/assets/images/projects/Pepsi.png",
+    title: "PEPSI APP",
+    description:
+      "This is a static web application inspired by the Pepsi website and connected to the original Pepsi website.",
+    liveDemo: "https://2404amol.github.io/PEPSI-WEBAPP/",
+    code: "https://github.com/2404AMOL/PEPSI-WEBAPP",
+  },
+
+  {
+    image: "/assets/images/projects/HmBox2023.png",
+    title: "HMBOX 2023",
+    description:
+      "The Tiffin Service Web App facilitates ordering and delivery of home-cooked meals and tiffin services.",
+    liveDemo: "https://hmbox2023.great-site.net/?i=1",
+    code: "#",
+  },
+
+  {
+    image: "/assets/images/projects/portfolio 1.jpg",
+    title: "PORTFOLIO 1",
+    description:
+      "This is my first portfolio website developed using ReactJs with animations and attractive UI design.",
+    liveDemo: "https://amol2404.netlify.app/",
+    code: "#",
+  },
+
+  {
+    image: "/assets/images/projects/Gymso-WebApp.png",
+    title: "GYMSO WEBAPP",
+    description:
+      "Responsive Gym Web Application designed using HTML and CSS Flexbox with attractive animations.",
+    liveDemo: "https://2404amol.github.io/GYMSO-WEBAPP/",
+    code: "https://github.com/2404AMOL/GYMSO-WEBAPP",
+  },
+
+  {
+    image: "/assets/images/projects/Cuda1.png",
+    title: "CUDA 1",
+    description:
+      "Responsive web application hosted using GitHub Pages.",
+    liveDemo: "https://2404amol.github.io/CUDA/",
+    code: "https://github.com/2404AMOL/CUDA",
+  },
+
+  {
+    image: "/assets/images/projects/Restro.png",
+    title: "RESTRO WEBAPP",
+    description:
+      "Restaurant landing page designed using HTML, CSS, and animations.",
+    liveDemo: "#",
+    code: "#",
+  },
+
+  {
+    image: "/assets/images/projects/IplSchedule23.png",
+    title: "IPL SCHEDULE 2023",
+    description:
+      "IPL Schedule 2023 web application developed using HTML tables.",
+    liveDemo: "https://iplschedule23.netlify.app/",
+    code:
+      "https://github.com/2404AMOL/WEB-TECH/tree/main/PROJECTS/PROJECT%2014",
+  },
+
+  {
+    image: "/assets/images/projects/Mobile Interface.png",
+    title: "MOBILE INTERFACE",
+    description:
+      "Mobile interface created using HTML and CSS with interactive real-time button actions.",
+    liveDemo: "#",
+    code: "#",
+  },
+];
+
+export default projectsData;
