401b946161249ddddb65e466512a278722d294ce
Amol Pawar <amolpawar.dev@gmail.com>
2026-05-26 17:36:11 +0530

    Changed SCSS Folder Structure and Removed the Ununsed File and Folder

--- src/pages/TechStack/index.TechStack.tsx
+++ src/pages/TechStack/index.TechStack.tsx
@@ -1,69 +1,24 @@
 "use client";
 
 import { techStackData } from "@/data/techStackData";
+import TechStackCards from "./components/TechStackCards";
 
 export default function TechStack() {
-
   return (
-
     <div className="container">
 
       {/* ================= SECTION TITLE ================= */}
-
       <div className="row">
-
         <div className="section-title">
-
           <h2>My Tech Stack</h2>
-
         </div>
-
       </div>
 
       {/* ================= TECH STACK ITEMS ================= */}
-
       <div className="row">
-
-        {
-          techStackData.map((tech, index) => (
-
-            <div
-              className="techstack-item padd-15"
-              key={index}
-            >
-
-              <div className="techstack-item-inner">
-
-                {/* ================= ICON ================= */}
-
-                <div className="icon">
-
-                  <i className={tech.icon}></i>
-
-                </div>
-
-                {/* ================= TITLE ================= */}
-
-                <h4>
-
-                  {tech.title}
-
-                </h4>
-
-                {/* ================= DESCRIPTION ================= */}
-
-                <p>
-
-                  {tech.description}
-
-                </p>
-
-              </div>
-
-            </div>
-          ))
-        }
-
+        {techStackData.map((tech, index) => (
+          <TechStackCards key={index} tech={tech} />
+        ))}
       </div>
 
     </div>
